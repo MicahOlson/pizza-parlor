5b2070f5f37974452526a36d86575dd0117dc6c6
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-29 10:11:20 -0800

    Add and test sumCost() method for size conditional

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.sumCost = function() {
+  if (this.size === "small") {
+    return 10;
+  } else if (this.size === "medium") {
+    return 15;
+  } else if (this.size === "large") {
+    return 20;
+  }
+}
