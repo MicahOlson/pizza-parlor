43fea3f5521a5946dba4acc736194cbea3c77b91
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-29 13:27:12 -0800

    Add UI logic to feed HTML order details

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,36 @@ function Pizza(size, toppings) {
 
 Pizza.prototype.sumCost = function() {
   let sizePrice;
-  if (this.size === "small") {
+  if (this.size === "Small") {
     sizePrice = 10;
-  } else if (this.size === "medium") {
+  } else if (this.size === "Medium") {
     sizePrice = 15;
-  } else if (this.size === "large") {
+  } else if (this.size === "Large") {
     sizePrice = 20;
   }
+  if (this.toppings.length < 1) {
+    return sizePrice;
+  } else {
   return sizePrice + (this.toppings.length * .75);
+  }
 }
 
 $(document).ready(function() {
-  $(".frmPizza").submit(function(event) {
+  $("form.frmPizza").submit(function(event) {
     event.preventDefault();
     const pizzaSize = $("input:radio[name=radPizza]:checked").val();
     let pizzaToppings = [];
     $("input:checkbox[name=chkToppings]:checked").each(function() {
       pizzaToppings.push($(this).val());
     });
+    
+    let customPizza = new Pizza(pizzaSize, pizzaToppings);
+
+    $("ul#orderToppings li").remove();
+    $("#orderSize").text(customPizza.size);
+    customPizza.toppings.forEach(function(topping) {
+      $("ul#orderToppings").append("<li>" + topping + "</li>");
+    });
+    $("#orderCost").text(customPizza.sumCost());
   });
 });
